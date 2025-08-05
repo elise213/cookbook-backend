1423a11cfe7e7a4b9611a56039acedbb0d0e3672
Mara <maracodes213@gmail.com>
2025-08-04 23:57:15 -0700

    saving

--- app.py
+++ app.py
@@ -27,7 +27,9 @@ def create_app():
 
     @app.after_request
     def apply_cors_headers(response):
-        response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
+        origin = request.headers.get("Origin")
+        if origin in ["http://localhost:3000", "https://zesty-phoenix-8cec46.netlify.app"]:
+            response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Credentials"] = "true"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
         response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
