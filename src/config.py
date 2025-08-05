fa32e7781eeab9d01082fad9ae99f593a3310545
Mara <maracodes213@gmail.com>
2025-08-05 13:01:49 -0700

    Connect to Render Postgres DB

--- src/config.py
+++ src/config.py
@@ -6,9 +6,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/db.sqlite3"
+    # SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    SQLALCHEMY_DATABASE_URI = "postgresql://cookbook_db_vn7f_user:VtUER8pZnIeUWqQAiZmQLR4mAPgluImc@dpg-d2963obuibrs73e8jhcg-a/cookbook_db_vn7f"
 
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_ACCESS_COOKIE_NAME = "access_token_cookie"
@@ -16,3 +17,4 @@ class Config:
     JWT_COOKIE_SAMESITE = "Lax"
     JWT_COOKIE_SECURE = True  # True ONLY if using HTTPS
     JWT_ACCESS_COOKIE_PATH = "/"
+
