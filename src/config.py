678be38b60aa58c3d47012583c9788bcec59eaa1
Mara <maracodes213@gmail.com>
2025-08-05 14:46:31 -0700

    s

--- src/config.py
+++ src/config.py
@@ -6,11 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret")
-    SQLALCHEMY_DATABASE_URI = "postgresql://cookbook_db_vn7f_user:VtUER8pZnIeUWqQAiZmQLR4mAPgluImc@dpg-d2963obuibrs73e8jhcg-a/cookbook_db_vn7f"
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///../instance/db.sqlite3"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
     JWT_TOKEN_LOCATION = ["cookies"]
     JWT_ACCESS_COOKIE_NAME = "access_token_cookie"
     JWT_COOKIE_CSRF_PROTECT = False
